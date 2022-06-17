b5638866a352a93fd0044fa066beb5bd9e6543ae
Magbo Charles <magboelochukwu@gmail.com>
2022-06-17 10:57:54 +0100

    second

--- I4G004124JJF/blog/models.py
+++ I4G004124JJF/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField("")
+    author = models.Foreignkey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add = True)
+    published_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
